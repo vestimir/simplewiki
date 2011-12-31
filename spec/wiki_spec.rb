5887415be731675dd120367bd277609f82ce737e
Vestimir Markov <vestimir@gmail.com>
2011-12-31 14:39:19 +0200

    reindent

--- spec/wiki_spec.rb
+++ spec/wiki_spec.rb
@@ -4,32 +4,32 @@ require 'rack/test'
 set :environment, :test
 
 def app
-    Sinatra::Application
+  Sinatra::Application
 end
 
 describe 'Wiki' do
-    include Rack::Test::Methods
+  include Rack::Test::Methods
 
-    it "should have a layout" do
-        get '/'
-        last_response.body.downcase.should match %r/<!doctype html>/i
-    end
+  it "should have a layout" do
+    get '/'
+    last_response.body.downcase.should match %r/<!doctype html>/i
+  end
 
-    it "should have homepage" do
-        get '/'
-        last_response.body.should match %r/<p>This <em>is<\/em> Markdown<\/p>/i
-    end
+  it "should have homepage" do
+    get '/'
+    last_response.body.should match %r/<p>This <em>is<\/em> Markdown<\/p>/i
+  end
 
-    it "should have slug page" do
-        get '/sample'
-        last_response.body.should match %r/<p>This is a page<\/p>/i
-    end
+  it "should have slug page" do
+    get '/sample'
+    last_response.body.should match %r/<p>This is a page<\/p>/i
+  end
 
-    it "should have table of contents" do
-        get '/contents'
-        read_dir(CONTENT).each do |f|
-            f.gsub! '.md', ''
-            last_response.body.should match %r/<a href="\/#{f}">/i
-        end
+  it "should have table of contents" do
+    get '/contents'
+    read_dir(CONTENT).each do |f|
+      f.gsub! '.md', ''
+      last_response.body.should match %r/<a href="\/#{f}">/i
     end
+  end
 end
--- wiki.rb
+++ wiki.rb
@@ -5,31 +5,31 @@ set :markdown, :layout_engine => :erb
 set :views, CONTENT
 
 def read_dir(dir)
-    files = []
-    Dir.entries(dir).each do |i|
-        files << i if not ['.', '..', 'layout.erb'].include? i and not i.match %r/^\./i
-    end
-    files
+  files = []
+  Dir.entries(dir).each do |i|
+    files << i if not ['.', '..', 'layout.erb'].include? i and not i.match %r/^\./i
+  end
+  files
 end
 
 get '/' do
-    markdown :homepage
+  markdown :homepage
 end
 
 get '/contents' do
-    contents = ''
-    read_dir(CONTENT).each do |f|
-        slug = f.gsub '.md', ''
-        contents += '<li><a href="/' + slug + '">' + slug + '</a></li>'
-    end
-    erb '<h1>Table of Contents</h1><ul>' + contents + '</ul>'
+  contents = ''
+  read_dir(CONTENT).each do |f|
+    slug = f.gsub '.md', ''
+    contents += '<li><a href="/' + slug + '">' + slug + '</a></li>'
+  end
+  erb '<h1>Table of Contents</h1><ul>' + contents + '</ul>'
 end
 
 get '/:page' do
-    begin
-        markdown params[:page].to_sym
-    rescue
-        #why no layout?
-        erb '<h1>Page Not Found</h1>'
-    end
+  begin
+    markdown params[:page].to_sym
+  rescue
+    #TODO: why no layout?
+    erb '<h1>Page Not Found</h1>'
+  end
 end
